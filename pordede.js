3fe693eebc947f5f8adf8c5621adf2148755c7de
deli0s <info.delios@gmail.com>
2016-09-22 00:01:14 +0200

    Update pordede.js

--- pordede.js
+++ pordede.js
@@ -1,2 +1,2 @@
-$.getScript('https://raw.githubusercontent.com/deli0s/js/master/pordede_ad.js', function() { });
-$.getScript('https://raw.githubusercontent.com/deli0s/js/master/pordede_custom.js', function() { });
+$.getScript('https://rawgit.com/deli0s/js/master/pordede_ad.js', function() { });
+$.getScript('https://rawgit.com/deli0s/js/master/pordede_custom.js', function() { });
