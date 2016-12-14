5ea0a469f2b75e1fbb174d050ad9b5b4e15a0502
deli0s <info.delios@gmail.com>
2016-12-14 02:07:04 +0100

    Update pordede_custom.js

--- pordede_custom.js
+++ pordede_custom.js
@@ -71,32 +71,27 @@ function edit2(){
 								var child_ep_i=episodes_lk[ep_i];
 								var ep_text=child_ep_i.innerHTML
 								var ep_season=ep_text.substring(0,ep_text.indexOf("x"));//nº season
-								var ep_ep=(ep_text.replace(ep_season+"x",""));//nº ep
+								var ep_ep=Number(ep_text.replace(ep_season+"x",""));//nº ep
 								var ep_start=data.indexOf("episodes-"+ep_season);
 								data=data.substr(ep_start,data.length-ep_start);
-								/*for (var pass_i=1;pass_i<ep_ep;pass_i++){
+								for (var pass_i=1;pass_i<ep_ep;pass_i++){
 									data=data.replace("modelContainer defaultPopup","");
 								}
 								var ep_start2=data.indexOf("modelContainer defaultPopup");
 								data=data.substr(ep_start2,data.length-ep_start2);
 								var ep_start3=data.indexOf("viewepisode");
 								data=data.substr(ep_start3+17,6);
-								var lk_ep="/links/viewepisode/id/"+data;*/
-								var rex = new RegExp('\\/links\/viewepisode\/id\/[0-9]*.{0,8}class="number">'+ep_ep);
-								var lk_ep_str = String(data.match(rex));
-								var lk_ep=lk_ep_str.replace('"><span class="number">'+ep_ep,'')
+								var lk_ep="/links/viewepisode/id/"+data;
 								if (child_ep_i.parentNode.getElementsByClassName("2-linky-winky").length<1){
-									if (lk_ep!==undefined && lk_ep!==null){
-										addLinks2(lk_ep,child_ep_i.parentNode);
-									}else{
-										console.log("ERROR: ep "+ep_text);
+									addLinks2(lk_ep,child_ep_i.parentNode);
+									if (data.length!=6){
+										console.log("ERROR: La id: "+data+", te mida: "+data.length);
 										var a_ep_no=child_ep_i.parentNode.getElementsByTagName("a")[8];
 										if (a_ep_no!==undefined && a_ep_no!==null){
 											a_ep_no.style.display="none";
 										}
 									}
 								}
-								//end html link
 							}
 						}
 					});
