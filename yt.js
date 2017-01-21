6c4dc7a19dab4272554dd200dff5a97fdc83d6a5
deli0s <info.delios@gmail.com>
2017-01-21 03:21:53 +0100

    Update yt.js

--- yt.js
+++ yt.js
@@ -5,23 +5,25 @@ function edit(){
 			var size_videos=videos.length;
 			for (var i_video=0; i_video<size_videos; i_video++){
 				var video=videos[i_video];
-				var watched=video.getElementsByClassName("resume-playback-progress-bar")[0];
-				if (existeix(watched)){console.log(i_video);
-					deleteByClass("yt-shelf-grid-item",i_video);
-					i_video--;
-				}else{
-					var late_h3=video.getElementsByTagName("h3")[0];
-					var late=(late_h3.innerHTML.indexOf("LATE MOTIV")>-1);
-					var late_david=(late_h3.innerHTML.indexOf("David Broncano")>-1);
-					var late_berto=(late_h3.innerHTML.indexOf("Berto Romero")>-1);
-					if (late){
-						if (!late_david && !late_berto){
-							deleteByClass("yt-shelf-grid-item",i_video);
-							i_video--;
+				if (existeix(video)){
+					var watched=video.getElementsByClassName("resume-playback-progress-bar")[0];
+					if (existeix(watched)){
+						deleteByClass("yt-shelf-grid-item",i_video);
+						i_video--;
+					}else{
+						var late_h3=video.getElementsByTagName("h3")[0];
+						var late=(late_h3.innerHTML.indexOf("LATE MOTIV")>-1);
+						var late_david=(late_h3.innerHTML.indexOf("David Broncano")>-1);
+						var late_berto=(late_h3.innerHTML.indexOf("Berto Romero")>-1);
+						if (late){
+							if (!late_david && !late_berto){
+								deleteByClass("yt-shelf-grid-item",i_video);
+								i_video--;
+							}
 						}
 					}
+				//console.log(i_video);
 				}
-			//console.log(i_video);
 			}
 		}
 	}
