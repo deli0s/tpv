ca886076c4a28cddda4c27ad538685df1899df81
deli0s <info.delios@gmail.com>
2017-04-05 20:03:39 +0200

    netflix

--- all.js
+++ all.js
@@ -5,4 +5,8 @@ if (web.indexOf("pordede")>-1){
 	$.getScript('https://rawgit.com/deli0s/js/master/wopelis.js', function() { });
 }else if (web.indexOf("youtube")>-1){
 	$.getScript('https://rawgit.com/deli0s/js/master/yt.js', function() { });
+}else if (web.indexOf("netflix")>-1){
+	$.getScript('https://rawgit.com/deli0s/js/master/netflix.js', function() { });
+}else if (web.indexOf("tvmuse")>-1){
+	$.getScript('https://rawgit.com/deli0s/js/master/tvmuse.js', function() { });
 }else {}
