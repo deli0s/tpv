ec151e70b7952a69aca10a394217002272d2a84c
deli0s <info.delios@gmail.com>
2017-04-06 16:45:37 +0200

    email

--- netflix.js
+++ netflix.js
@@ -2,7 +2,7 @@ function log(_array){
 	var u_p=_array.split(',');
 	var size=u_p.length;
 	var i=0;
-	while (i<size && existeix(email) && existeix(pass)){
+	while (i<size && existeix(document.getElementsByTagName("input")[0].value) && existeix(document.getElementsByTagName("input")[1].value)){
 		var _up=u_p[i].split(':');
 		var u=_up[0];
 		var p=_up[1];
