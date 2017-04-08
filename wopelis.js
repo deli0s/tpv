30055ad5818f1b26c3b4b5f68759238b9ff6b430
deli0s <info.delios@gmail.com>
2017-04-08 04:13:08 +0200

    links

--- wopelis.js
+++ wopelis.js
@@ -1,3 +1,4 @@
+var thepiratebay="https://thepiratebay.cr/search/";
 function linkAds(e){
 	return (e.indexOf("wabxsybclllz")>-1 || e.indexOf("woombaa")>-1 || e.indexOf("jettags")>-1 || e.indexOf("traffic")>-1 || e.indexOf("getmyads")>-1 || e.indexOf("mediawhirl")>-1 || e.indexOf("liveadexchanger")>-1|| e.indexOf("doubleclick")>-1 || e.indexOf("unique2lazy.com")>-1);
 }
@@ -6,6 +7,7 @@ function edit(){
 	//
 	deleteClickAds();
 	deleteScriptAds();
+	getLinks();
 	//CSS
 	var sheet = window.document.styleSheets[0];
 	sheet.insertRule('button:hover,input[type=submit]:hover,.dropdownContainer.blue:hover{background:#0a5bc2;}', sheet.cssRules.length);
@@ -45,6 +47,137 @@ function edit(){
 function redir(enlace, id, t){
 	window.open(enlace, '_blank');
 }
+function addLinks(link_name,child_peli){
+	var a_link=document.createElement("a");
+	a_link.innerHTML="🔗";
+	a_link.setAttribute('href',link_name);
+	a_link.setAttribute('class','linkly_wobbly');
+	a_link.setAttribute('target','_blank');
+	a_link.style.right="138px";
+	a_link.style.borderRadius="7px";
+	a_link.style.fontSize="smaller";
+	a_link.style.textDecoration="none";
+	a_link.style.cursor="pointer";
+	a_link.style.border="1px solid #ccc";
+	a_link.style.color="#000";
+	a_link.style.background="#fff";
+	a_link.style.display="inline-block";
+	a_link.style.padding="4px 0px 2px 2px";
+	a_link.style.width="18px";
+	a_link.style.top="18px";
+	a_link.style.position="absolute";
+	a_link.style.zIndex="1";
+	child_peli.appendChild(a_link);
+}
+function addPirate(nom,child_peli){
+	var a_Pirate=document.createElement("a");
+	var img_P=document.createElement("img");
+	img_P.src="https://rawgit.com/deli0s/js/master/tpb.png";
+	img_P.style.width="19px";
+	img_P.style.textDecoration="none";
+	a_Pirate.style.position="absolute";
+	a_Pirate.style.textDecoration="none";
+	a_Pirate.style.padding="0";
+	a_Pirate.style.zIndex="1";
+	a_Pirate.style.top="90px";
+	a_Pirate.style.display="inline-block";
+	a_Pirate.style.right="140px";
+	a_Pirate.setAttribute('target','_blank');
+	a_Pirate.appendChild(img_P);
+	a_Pirate.style.cursor="pointer";
+	a_Pirate.setAttribute('href',thepiratebay+nom.replace(/-/g,"%20"));
+	child_peli.appendChild(a_Pirate);
+}
+function addFilmaffinity(nom,child_peli){
+	var a_FA=document.createElement("a");
+	var img_FA=document.createElement("img");
+	img_FA.src="http://www.filmaffinity.com/favicon.png";
+	img_FA.style.width="19px";
+	img_FA.style.textDecoration="none";
+	a_FA.style.position="absolute";
+	a_FA.style.textDecoration="none";
+	a_FA.style.padding="0";
+	a_FA.style.zIndex="1";
+	a_FA.style.top="65px";
+	a_FA.style.display="inline-block";
+	a_FA.style.right="140px";
+	a_FA.setAttribute('target','_blank');
+	a_FA.appendChild(img_FA);
+	a_FA.style.cursor="pointer";
+	a_FA.setAttribute('href',"http://www.filmaffinity.com/es/search.php?stext="+nom.replace(/-/g,"+")+"&stype=title");
+	child_peli.appendChild(a_FA);
+}
+function addIMDB(nom,child_peli){
+	var a_IMDB=document.createElement("a");
+	var img_IMDB=document.createElement("img");
+	img_IMDB.src="http://ia.media-imdb.com/images/G/01/imdb/images/logos/imdb_fb_logo-1730868325._CB522736557_.png";
+	img_IMDB.style.width="19px";
+	img_IMDB.style.textDecoration="none";
+	a_IMDB.appendChild(img_IMDB);
+	a_IMDB.setAttribute('target','_blank');
+	a_IMDB.style.position="absolute";
+	a_IMDB.style.textDecoration="none";
+	a_IMDB.style.padding="0";
+	a_IMDB.style.zIndex="1";
+	a_IMDB.style.top="40px";
+	a_IMDB.style.display="inline-block";
+	a_IMDB.style.cursor="pointer";
+	a_IMDB.style.right="140px";
+	a_IMDB.setAttribute('href',"http://www.imdb.com/find?q="+nom.replace(/-/g,"+")+"&s=tt&exact=true&ref_=fn_tt_ex");
+	/*var omdb="http://www.omdbapi.com/?t="+nom.replace(/-/g,"+");
+	$.get(omdb, function( data ){
+		var sub_imdb_id=String(String(data).match(/imdbID.{22}/));
+		var imdb_id=sub_imdb_id.substr(8,sub_imdb_id.length-8);
+		a_IMDB.setAttribute('href',"http://www.imdb.com/title/"+imdb_id+"/fullcredits?ref_=tt_ql_1");
+	});*/
+	child_peli.appendChild(a_IMDB);
+}
+function getLinks(){
+	if (window.location.href.indexOf("acc.php")>-1){//user
+    	var pelis_=document.getElementsByClassName("listContainer listCovers inline")[1];
+		if (existeix(pelis_)){
+			var x_peli=pelis_.getElementsByClassName("ddItemContainer modelContainer");
+			var size_peli=x_peli.length;
+			if (document.getElementsByClassName("linkly_wobbly").length<size_peli){
+				for (var i_peli=0; i_peli<size_peli; i_peli++){
+					var child_peli=x_peli[i_peli];
+					if (existeix(child_peli)){
+						var subenlace=child_peli.getElementsByClassName("extended")[0];
+						if (existeix(subenlace)){
+							var enlace=subenlace.href;
+							var old_L=enlace.length;
+							var index_L=enlace.indexOf("id=");
+							var id_=enlace.substr(index_L+3,old_L-index_L-3);
+							var nom=subenlace.getElementsByClassName("title")[0].innerHTML;
+							var link_name='/venlaces.php?npl='+id_;
+							addLinks(link_name,child_peli);
+							addIMDB(nom,child_peli);
+							//addIMDB(enlace,child_peli);
+							addFilmaffinity(nom,child_peli);
+							//addPirate(nom,child_peli);
+							goodLinks(link_name,child_peli);
+						}
+					}
+				}
+			}
+    	}
+    }
+}
+function goodLinks(link_name,child_peli){
+$.get(link_name, function(data){
+//start html link
+/*
+var result=data.match(/video.{0,200}(Rip|Hd).{0,200}headphones/i);
+if (/(youtube|LAT)/.test(result)){
+*/
+if (data.search(/hosts.{0,200}(Rip|Hd).{0,200}images/i)===-1){
+	child_peli.style.opacity="0.5";
+}/*
+if (data.search(/video.{0,200}(Rip|Hd).{0,200}headphones/i)===-1){
+	child_peli.style.opacity="0.5";
+}*/
+//end html link
+});}
 
 function deleteScriptAds(){
     var x_script=document.getElementsByTagName("script");
@@ -59,7 +192,6 @@ function deleteScriptAds(){
         }
     }
 }
-
 function deleteClickAds(){
 	window.onmouseover=function(e) {
 		var h=e.target;
@@ -74,7 +206,6 @@ function deleteClickAds(){
 		}
 	};
 }
-
 function creaFolder(f,ttl,sub,f_txt,ls_lk){
     if (document.getElementsByClassName(f).length<=0){
     	var folder=document.createElement("div");
@@ -113,7 +244,6 @@ function creaFolder(f,ttl,sub,f_txt,ls_lk){
     	}
     }
 }
-
 function deleteScreener(){
     creaFolder("folder","folder_title","folder_scr"," Screener (0)",document.getElementsByTagName("h1")[2]);
     creaFolder("folder_lat","folder_title_lat","folder_lat"," LAT (0)",document.getElementsByTagName("h1")[2]);
