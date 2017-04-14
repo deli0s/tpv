760354fa980843d43feb16fef87dec6ad0055f03
deli0s <info.delios@gmail.com>
2017-04-14 02:56:14 +0200

    IMDb icon

--- wopelis.js
+++ wopelis.js
@@ -174,7 +174,7 @@ function addFilmaffinity(nom,child_peli,absolute){
 function addIMDB(nom,child_peli,absolute){
 	var a_IMDB=document.createElement("a");
 	var img_IMDB=document.createElement("img");
-	img_IMDB.src="http://ia.media-imdb.com/images/G/01/imdb/images/favicon-2165806970._CB522736556_.ico";
+	img_IMDB.src="http://ia.media-imdb.com/images/G/01/imdb/images/logos/imdb_fb_logo-1730868325._CB522736557_.png";
 	img_IMDB.style.textDecoration="none";
 	a_IMDB.appendChild(img_IMDB);
 	a_IMDB.setAttribute('class','linkly_wobbly');
