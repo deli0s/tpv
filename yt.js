64089fcc934fff5a9410a131757775ba99318e82
deli0s <info.delios@gmail.com>
2017-05-31 04:46:56 +0200

    h3_array

--- yt.js
+++ yt.js
@@ -7,7 +7,7 @@ function edit(){
 				var video=videos[i_video];
 				if (existeix(video)){
 					var watched=video.getElementsByClassName("resume-playback-progress-bar")[0];
-					if (existeix(watched) || t_(video,"Late Motiv","Broncano") || t_(video,"Late Motiv","Romero") || t_(video,"Late Motiv","ignatius") || t_(video,"TERRAT","NADIE SABE NADA") || t_(video,"manga","Titan") || t_(video,"Oh! My LOL","Moderna ") || t_(video,"Jimmy Fallon","Hashtags") || t_(video,"WIRED","Answer the Web") || t_(video,"Pazos64","Cuidao Ahí")){
+					if (existeix(watched) || t_(video,"Late Motiv","Broncano,Romero,ignatius") || t_(video,"TERRAT","NADIE SABE NADA") || t_(video,"manga","Titan") || t_(video,"Oh! My LOL","Moderna ") || t_(video,"Jimmy Fallon","Hashtags") || t_(video,"WIRED","Answer the Web") || t_(video,"Pazos64","Cuidao Ahí")){
 						deleteByClass("yt-shelf-grid-item",i_video);
 						i_video--;
 					}
@@ -18,12 +18,24 @@ function edit(){
 	}
 }
 
-function t_(video,title_str,h3_str){//title
+function t_(video,title_str,h3_array){//title
 	var title_=video.getElementsByTagName("a")[2];
 	var h3_=video.getElementsByTagName("h3")[0];
 	var title_2=(title_.innerHTML.toLowerCase().indexOf(title_str.toLowerCase())>-1);
+	if (!title_2) return false;
+	var h3_spl=h3_array.split(",");
+	if (h3_spl.length>1){
+		var try_n=0;
+		var trobat=false;
+		do{
+			trobat=(h3_.innerHTML.toLowerCase().indexOf(h3_spl[try_n].toLowerCase())>-1);
+			try_n++;
+		}while (!trobat && try_n<h3_spl.length)
+		return (!trobat);
+	}
+	h3_str=h3_spl[0];
 	var h3_2=(h3_.innerHTML.toLowerCase().indexOf(h3_str.toLowerCase())>-1);
-	return (title_2 && !h3_2);
+	return (!h3_2);
 }
 
 function existeix(nom){
