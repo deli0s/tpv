8bf30ad3ded33a4fe6cc4901fb4a9d45f9981e8b
deli0s <info.delios@gmail.com>
2017-07-20 00:16:24 +0200

    Add files via upload

---
+++ plusdede.js
@@ -0,0 +1,53 @@
+function edit(){
+	if (window.location.href.indexOf("aporte") > -1){
+		var visitar=document.getElementsByClassName("visit-buttons")[0];
+		if (existeix(visitar)){
+			var visitar_a=visitar.getElementsByTagName("a")[0];
+			if (existeix(visitar_a)){
+				window.location.replace(visitar_a.href);
+			}
+		}
+	}
+}
+function existeix(nom){
+	return (nom!==undefined && nom!==null);
+}
+function deleteById(nom){
+	var childAdv=document.getElementById(nom);
+	if (childAdv!==null && childAdv!==undefined){
+		var pareAdv=childAdv.parentNode;
+		pareAdv.removeChild(childAdv);
+	}
+}
+function deleteByClass(nom,num){
+	var childClass=document.getElementsByClassName(nom)[num];
+    if (childClass!==null && childClass!==undefined){
+        var pareClass=childClass.parentNode;
+        pareClass.removeChild(childClass);
+    }
+}
+function setCookie(cname, cvalue, exdays){
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+function getCookie(cname){
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for (var i=0; i<ca.length; i++){
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1);
+		if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+	}
+	return "";
+}
+function reload(){
+	try {
+        edit();
+    }catch(err) {
+        console.log(err);
+    }
+}
+document.body.onclick=reload;
+document.body.addEventListener("wheel",reload);
